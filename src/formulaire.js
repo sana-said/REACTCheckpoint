225f75a72d4831a0c4436698f961cf43681c15ec
sana-said <sana.said.aissa@gmail.com>
2020-11-02 09:37:35 +0100

    first commit

---
+++ src/formulaire.js
@@ -0,0 +1,13 @@
+const Inscription = () => {
+  return (
+    <form>
+      <h1>Veuillez remplir ce Formulaire pour votre inscription</h1>
+          <div><label class="labels" for="">NOM:</label>  <input type="text" name="nom" /></div>
+          <div><label class="labels" for="">PRENOM:</label>  <input type="text" name="prenom" /></div>
+          
+          
+</form>
+  );
+};
+
+export default Inscription ;
